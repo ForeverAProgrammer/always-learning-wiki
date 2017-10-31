fca481464162d0ee9de544ea19e1736f28a8b63f
KiraRenee <kristina.haynes@hotmail.com>
2017-10-31 15:15:02 -0500

    Added simple JUnit demo

---
+++ demos/junit/simple-junit-unit-test/src/test/java/codes/kristina/test/BasicUnitTest.java
@@ -0,0 +1,14 @@
+package codes.kristina.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BasicUnitTest {
+
+	@Test
+	void myFirstTest() {
+		assertEquals(4, 2 + 2);
+	}
+
+}
